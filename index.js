e75f97e3db66190a7488a9f69775ca9e9dd3bc76
Saim Rizvi <shr2mz@gmail.com>
2021-09-25 21:02:36 -0400

    commit tree 5fa75e8af405045e555c58a66dfaa2c6cdb32a34

---
+++ index.js
@@ -0,0 +1,14 @@
+// const add = (parameter1, parameter2) => parameter1 + parameter2
+// console.log(add(5,6))
+
+// const num = [1,2,3]
+// const square = num.map(x => x ** 2)
+// console.log(square)
+
+const divide = function() {
+    return 2000/100 
+}
+
+const square = x => x * 2 
+
+const add = (a,b) => a + b 
